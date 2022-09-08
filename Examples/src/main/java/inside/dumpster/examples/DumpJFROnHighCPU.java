b52975d65bdcbedf379e664c10b64eded46b76a3
Joakim Nordström <joakim.nordstrom@oracle.com>
2022-09-08 12:45:46 +0200

    Fixed DumpJFROnHighCPU

--- Examples/src/main/java/inside/dumpster/examples/DumpJFROnHighCPU.java
+++ Examples/src/main/java/inside/dumpster/examples/DumpJFROnHighCPU.java
@@ -3,51 +3,59 @@
  */
 package inside.dumpster.examples;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import jdk.jfr.Configuration;
-import jdk.jfr.consumer.RecordedEvent;
 import jdk.jfr.consumer.RecordingStream;
 
 /**
- *
+ * Example to show how to dump a JFR file when the CPU load is too high. 
+ * A recording stream is used to monitor the CPULoad event and the trigger 
+ * a JFR file top be dumped when the CPU load is above a certain level.
+ * is too high.
+ * 
  * @author Joakim Nordstrom joakim.nordstrom@oracle.com
  */
 public class DumpJFROnHighCPU {
+  public static final double CPU_LEVEL_THRESHOLD = 0.3;
   
   public static void main(String[] args) throws Exception {
     Configuration c = Configuration.getConfiguration("default");
-    
-    // create a recording stream using the "default" configuration
+    // create a recording stream using the "default" JFC configuration
     try (RecordingStream rs = new RecordingStream(c)) {
+      
       rs.onEvent("jdk.CPULoad", (event) -> {
+        // get CPU measurement: "jvmSystem", "jvmUser" or "machineTotal"
         float cpuLevel = event.getFloat("machineTotal");
-        System.out.println("cpu: "+cpuLevel);
-        if(cpuLevel > 0.3) {
+        System.out.println(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())+ ": cpu: "+cpuLevel);
+        
+        if(cpuLevel > CPU_LEVEL_THRESHOLD) {
           try {
-            Path jfrFilePath = File.createTempFile("highcpu", "jfr").toPath();
-            rs.dump(jfrFilePath);
-            System.out.println("JFR file dumped: "+jfrFilePath);
+            final Path jfrFilePath = generateJFRFilename();
+      
+            // This will limit a new JFR file to every minute,
+            // you'd likely want something smarter here.
+            if( ! jfrFilePath.toFile().exists()) {
+              rs.dump(jfrFilePath);
+              System.out.println("JFR file dumped due to high CPU: " + jfrFilePath);
+            }
+            
           } catch (IOException ex) {
             ex.printStackTrace();
           }
         }
       });
-      rs.startAsync(); 
       
-      double val1 = 9;
-      double val2;
-      while(true) {
-        val2 = Math.sqrt(val1);
-        val1 = Math.IEEEremainder(val1, val2);
-        
-      }
-      
+      rs.start();
     }
+  }
+  
+  private static Path generateJFRFilename() {
+    final String tmpdir = System.getProperty("java.io.tmpdir");
+    final String filename = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
+    return Path.of(tmpdir, filename + ".jfr");
+            
   }
 }
