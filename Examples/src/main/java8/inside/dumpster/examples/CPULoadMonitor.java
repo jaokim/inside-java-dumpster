b8af2408d116b3b18ceb1c303872952dbb106eda
Joakim Nordström <joakim.nordstrom@oracle.com>
2022-09-22 12:54:46 +0200

    Added cpu load monitor for both 8 and 17.

---
+++ Examples/src/main/java8/inside/dumpster/examples/CPULoadMonitor.java
@@ -0,0 +1,29 @@
+/*
+ * 
+ */
+package inside.dumpster.examples;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.function.DoublePredicate;
+import java.util.function.Supplier;
+
+/**
+ * 
+ * @author Joakim Nordstrom joakim.nordstrom@oracle.com
+ */
+public interface CPULoadMonitor {
+  enum Attribute {
+    SystemLoad, 
+    ProcessLoad
+  }
+  /**
+   * Add a CPU level monitor
+   * @param cpuLevelTester returns true to dump a JFR recording.
+   * @param jfrRecordingDestination supplies a file if a recording should be 
+   * dumped. Should be used to limit the number of files created.
+   * @throws Exception 
+   */
+  
+  void monitor(DoublePredicate cpuLevelTester, Supplier<Optional<File>> jfrRecordingDestination) throws Exception;
+}
--- Examples/src/main/java8/inside/dumpster/examples/DumpJFROnHighCPU.java
+++ Examples/src/main/java8/inside/dumpster/examples/DumpJFROnHighCPU.java
@@ -3,12 +3,12 @@
  */
 package inside.dumpster.examples;
 
-import java.io.IOException;
-import java.nio.file.Path;
+import java.io.File;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import jdk.jfr.Configuration;
-import jdk.jfr.consumer.RecordingStream;
+import java.util.Optional;
 
 /**
  * Example to show how to dump a JFR file when the CPU load is too high. 
@@ -21,42 +21,30 @@ public class DumpJFROnHighCPU {
   public static final double CPU_LEVEL_THRESHOLD = 0.3;
   
   public static void main(String[] args) throws Exception {
-    Configuration c = Configuration.getConfiguration("default");
-    // create a recording stream using the "default" JFC configuration
-    try (RecordingStream rs = new RecordingStream(c)) {
-      
-      rs.onEvent("jdk.CPULoad", (event) -> {
-        // get CPU measurement: "jvmSystem", "jvmUser" or "machineTotal"
-        float cpuLevel = event.getFloat("machineTotal");
-        
-        if(cpuLevel > CPU_LEVEL_THRESHOLD) {
-          System.out.println("High CPU level noticed: "+cpuLevel);
-          try {
-            final Path jfrFilePath = generateJFRFilename();
-      
-            // This will limit a new JFR file to every minute,
-            // you'd likely want something smarter here.
-            if( ! jfrFilePath.toFile().exists()) {
-              rs.dump(jfrFilePath);
-              System.out.println("JFR file dumped due to high CPU: " + jfrFilePath);
-            }
-            
-          } catch (IOException ex) {
-            ex.printStackTrace();
-          }
-        } else {
-          System.out.println("CPU level ok: "+cpuLevel);
-        
-        }
-      });
-      
-      rs.start();
-    }
+    CPULoadMonitor cpuLoad = new CPULoadMonitorImpl();
+    cpuLoad.monitor(cpuLevel -> cpuLevel > CPU_LEVEL_THRESHOLD, 
+            () -> jfrDumpDestination());
   }
+
+  private static final int DUMPINTERVAL_SECONDS = 60;
   
-  private static Path generateJFRFilename() {
-    final String tmpdir = System.getProperty("java.io.tmpdir");
-    final String filename = new SimpleDateFormat("yyyyMMddHHmm").format(new Date());
-    return Path.of(tmpdir, filename + ".jfr");
+  /** Keeps track of last dumped JFR file. */
+  private static Date lastJFRDump = new Date();
+  
+  /**
+   * Returns an optional file to dump the JFR recording. 
+   * @return 
+   */
+  private synchronized static Optional<File> jfrDumpDestination() {
+    final Date now = new Date();
+    if(now.after(lastJFRDump)) {
+      final String tmpdir = System.getProperty("java.io.tmpdir");
+      final String filename = new SimpleDateFormat("yyyyMMddHHmm").format(now);
+      lastJFRDump = new Date(now.getTime() + DUMPINTERVAL_SECONDS * 1000);
+      final File jfrFile = new File(tmpdir, filename + ".jfr");
+      return Optional.of(jfrFile);
+    } else {
+      return Optional.empty();
+    }
   }
 }
