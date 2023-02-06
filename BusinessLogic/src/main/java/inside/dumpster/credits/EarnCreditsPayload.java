44383e18faae9dee7c2ad0f6df755bc17e876be4
Joakim Nordström <joakim.nordstrom@oracle.com>
2023-02-06 17:08:17 +0100

    Added service

---
+++ BusinessLogic/src/main/java/inside/dumpster/credits/EarnCreditsPayload.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ */
+package inside.dumpster.credits;
+
+import inside.dumpster.client.Payload;
+
+/**
+ *
+ * @author Joakim Nordstrom joakim.nordstrom@oracle.com
+ */
+public class EarnCreditsPayload extends Payload {
+  
+}
