f8c8bb79a34e0b93b0811d5ddb4090e50ef3c7e2
Joakim Nordström <joakim.nordstrom@oracle.com>
2023-09-12 11:50:02 +0200

    Minor re-naming and documentation.

--- BusinessLogic/BusinessLogic-8/src/main/java/inside/dumpster/bl/BusinessLogicServiceWrapper.java
+++ BusinessLogic/BusinessLogic-8/src/main/java/inside/dumpster/bl/BusinessLogicServiceWrapper.java
@@ -9,23 +9,26 @@ import inside.dumpster.client.Payload;
 import inside.dumpster.client.Result;
 import inside.dumpster.client.impl.Helper;
 import inside.dumpster.monitoring.event.ServiceInvocation;
+import java.util.Random;
 import java.util.UUID;
 
 /**
  * Wrapper for a business logic service to facilitate service mapping. This
  * only serves to lessen the boilerplate code needed to map the network logs
- * to service calls.
- * This construct is only used for the mimicked business application -- this
- * is -not- a viable design pattern to take inspiration from.
- * You've been warned!
+ * to service calls.This construct is only used for the mimicked business
+ * application -- this is -not- a viable design pattern to take inspiration
+ * from. You've been warned!
  *
  * @author Joakim Nordstrom joakim.nordstrom@oracle.com
+ * @param <SpecificPayload> class extending Payload
+ * @param <SpecificResult> class extending Result
  */
-public class BusinessLogicServiceWrapper<P extends Payload, R extends Result> {
+public class BusinessLogicServiceWrapper<SpecificPayload extends Payload, SpecificResult extends Result> {
   private final Authenticator authenticator = new Authenticator();
-  private final BusinessLogicService<P, R> service;
+  private final BusinessLogicService<SpecificPayload, SpecificResult> service;
+  private static final Random random = new Random();
 
-  BusinessLogicServiceWrapper(BusinessLogicService<P, R> service) {
+  BusinessLogicServiceWrapper(BusinessLogicService<SpecificPayload, SpecificResult> service) {
     this.service = service;
   }
 
@@ -35,19 +38,17 @@ public class BusinessLogicServiceWrapper<P extends Payload, R extends Result> {
    * @return
    * @throws BusinessLogicException
    */
-  public R invoke(Payload payload) throws BusinessLogicException {
-    payload.setTransactionId(UUID.randomUUID().toString());
+  public SpecificResult invoke(Payload payload) throws BusinessLogicException {
+    payload.setTransactionId(UUID.fromString(String.valueOf(random.nextGaussian())).toString());
     User user = authenticator.getLoggedInUser();
     ServiceInvocation serviceInvocation = new ServiceInvocation();
     serviceInvocation.serviceClass = service.getClass();
     serviceInvocation.registerPayloadData(payload);
     serviceInvocation.begin();
 
+    SpecificPayload convertedPayload = Helper.convertPayload(this.service.payloadClass, payload);
 
-    P convertedPayload = Helper.convertPayload(this.service.payloadClass, payload);
-    
-
-    R result = service.invoke(convertedPayload);
+    SpecificResult result = service.invoke(convertedPayload);
 
     serviceInvocation.end();
     serviceInvocation.commit();
