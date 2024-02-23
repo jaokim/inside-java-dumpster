2d20158f5f5e64af742d84eafe275388b48b445e
Joakim Nordström <joakim.nordstrom@oracle.com>
2024-02-23 11:17:17 +0100

    Some stuff added.

--- MicronautServer/src/main/java/inside/dumpster/micronaut/MicronautController.java
+++ MicronautServer/src/main/java/inside/dumpster/micronaut/MicronautController.java
@@ -4,7 +4,11 @@
 package inside.dumpster.micronaut;
 
 import inside.dumpster.bl.BusinessLogicException;
+import inside.dumpster.bl.BusinessLogicFactory;
 import inside.dumpster.bl.BusinessLogicServiceWrapper;
+import inside.dumpster.bl.auth.Authenticator;
+import inside.dumpster.bl.auth.MustAcceptCookiesError;
+import inside.dumpster.bl.auth.User;
 import inside.dumpster.client.Payload;
 import inside.dumpster.client.Result;
 import io.micronaut.core.annotation.Nullable;
@@ -14,6 +18,8 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import java.io.ByteArrayInputStream;
+import java.util.UUID;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,50 +29,65 @@ import org.slf4j.LoggerFactory;
  */
 @Controller("/business")
 public class MicronautController {
-      static final int MAX_LIMIT;// = 128; /* 0 MAX means disabled */
-    static {
-        int limit = 16384;
-        try {
-            String envLimit = System.getenv("JDK_JFR_INTERNAL_STRINGPOOL_MAX_LIMIT");
-            if (envLimit != null && envLimit.matches("[0-9]+")) {
-                limit = Integer.valueOf(envLimit);
-            }
-        } finally {
-            MAX_LIMIT = limit;
-        }
-}
-    private static Logger logger = LoggerFactory.getLogger(MicronautController.class);
 
-    private final MicronautService service;
+  private final Authenticator authenticator = new Authenticator();
+  private final BusinessLogicFactory factory = new BusinessLogicFactory();
 
-    public MicronautController(MicronautService service) {
-        this.service = service;
-        System.getenv("STRIGN");
-    }
+  private static final Logger logger = LoggerFactory.getLogger(MicronautController.class);
 
-    @Post(value = "/dest/{destination}/{srcDevice}/{srcDeviceID}/{srcPort}/{dstDevice}/{dstDeviceId}/{dstPort}/{protocol}", consumes = MediaType.APPLICATION_OCTET_STREAM)
-    public Result destination(@Nullable @Body byte[] payload, String destination, String srcDevice, String srcDeviceID, String srcPort, String dstDevice, String dstDeviceId, String dstPort, String protocol) throws BusinessLogicException {
+  private final MicronautService service;
+
+  public MicronautController(MicronautService service) {
+    this.service = service;
+    System.getenv("STRIGN");
+  }
+
+  @Post(value = "/dest/{destination}/{srcDevice}/{srcDeviceID}/{srcPort}/{dstDevice}/{dstDeviceId}/{dstPort}/{protocol}", consumes = MediaType.APPLICATION_OCTET_STREAM)
+  public Result destination(@Nullable @Body byte[] payload, String destination, String srcDevice, String srcDeviceID, String srcPort, String dstDevice, String dstDeviceId, String dstPort, String protocol) throws BusinessLogicException {
 
 //        HttpRequest<InputStream> httpRequest
-      Payload payload2 = new Payload("0", destination, srcDevice, dstDevice, protocol, srcPort, dstPort, srcPort, dstPort, srcPort, dstPort);
-      payload2.setInputStream(payload != null ? new ByteArrayInputStream(payload) : null);
+    Payload payload2 = new Payload("0", destination, srcDevice, dstDevice, protocol, srcPort, dstPort, srcPort, dstPort, srcPort, dstPort);
+    payload2.setInputStream(payload != null ? new ByteArrayInputStream(payload) : null);
 
-        BusinessLogicServiceWrapper businessLogicService = service.getService(destination);
-        logger.info("POST: "+destination+ " ");
-        logger.debug("POST: "+destination+ " ");
-        return businessLogicService.invoke(payload2);
-    }
+    BusinessLogicServiceWrapper businessLogicService = service.getService(destination);
+    logger.info("POST: " + destination + " ");
+    logger.debug("POST: " + destination + " ");
+    return businessLogicService.invoke(payload2);
+  }
 
-    @Get("/dest/{destination}/{srcDevice}/{srcDeviceID}/{srcPort}/{dstDevice}/{dstDeviceId}/{dstPort}/{protocol}")
-    public Result destination(String destination, String srcDevice, String srcDeviceID, String srcPort, String dstDevice, String dstDeviceId, String dstPort, String protocol) throws BusinessLogicException {
+  @Get("/dest/{destination}/{srcDevice}/{srcDeviceID}/{srcPort}/{dstDevice}/{dstDeviceId}/{dstPort}/{protocol}")
+  public Result destination(String destination, String srcDevice, String srcDeviceID, String srcPort, String dstDevice, String dstDeviceId, String dstPort, String protocol) throws BusinessLogicException {
+    return doRequest(destination, srcDevice, srcDeviceID, srcPort, dstDevice, dstDeviceId, dstPort, protocol);
+
+  }
+
+  private Result doRequest(String destination, String srcDevice, String srcDeviceID, String srcPort, String dstDevice, String dstDeviceId, String dstPort, String protocol) {
+    final User user;
+
+    try {
+      user = authenticator.authenticateUser("AUTH", UUID.randomUUID().toString(), null, new Object(), null);
+
       Payload payload = new Payload(null, destination, srcDevice, dstDevice, protocol, srcPort, dstPort, srcPort, dstPort, srcPort, dstPort);
       return service.getService(destination).invoke(payload);
-    }
+//      if (user.isCookieAccepted()) {
+//        authenticator.reauthenticate(user);
+//      }
 
-    @Get(value="/hello", produces = MediaType.TEXT_PLAIN)
-    public String hello() throws BusinessLogicException {
-        logger.info("POST: Everyone says hi ");
-        logger.debug("POST: hi h hi hi ");
-        return "Hi there!";
+
+    } catch (MustAcceptCookiesError ex) {
+
+    } catch (BusinessLogicException ex) {
+      java.util.logging.Logger.getLogger(MicronautController.class.getName()).log(Level.SEVERE, null, ex);
+    } finally {
+      authenticator.clearSession();
     }
+    return null;
+  }
+
+  @Get(value = "/hello", produces = MediaType.TEXT_PLAIN)
+  public String hello() throws BusinessLogicException {
+    logger.info("POST: Everyone says hi ");
+    logger.debug("POST: hi h hi hi ");
+    return "Hi there!";
+  }
 }
