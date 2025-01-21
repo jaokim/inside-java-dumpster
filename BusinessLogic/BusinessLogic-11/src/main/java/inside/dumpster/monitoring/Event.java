31e16f902fb6701e70f845f8844d44cea704e3ad
Joakim Nordström <joakim.nordstrom@oracle.com>
2025-01-21 09:46:50 +0100

    Scripts and some other stuff

---
+++ BusinessLogic/BusinessLogic-11/src/main/java/inside/dumpster/monitoring/Event.java
@@ -0,0 +1,14 @@
+/*
+ *
+ */
+package inside.dumpster.monitoring;
+
+/**
+ *
+ * @author Joakim Nordstrom joakim.nordstrom@oracle.com
+ */
+public class Event {
+  public void begin() {}
+  public void end() {}
+  public void commit() {}
+}
