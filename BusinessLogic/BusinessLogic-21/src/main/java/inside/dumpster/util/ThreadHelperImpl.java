ad1f6db9b0b9fab7512d2720b22740a2b7aadb2b
Joakim Nordström <joakim.nordstrom@oracle.com>
2025-03-11 11:15:28 +0100

    BL 21 updates

--- BusinessLogic/BusinessLogic-21/src/main/java/inside/dumpster/util/ThreadHelperImpl.java
+++ BusinessLogic/BusinessLogic-21/src/main/java/inside/dumpster/util/ThreadHelperImpl.java
@@ -3,19 +3,29 @@
  */
 package inside.dumpster.util;
 
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
 /**
  *
  * @author Joakim Nordstrom joakim.nordstrom@oracle.com
  */
-class ThreadHelperImpl implements ThreadHelper {
-
+class ThreadHelperImpl implements ThreadFactory {
+  private final Supplier<String> keysupplier;
+  private final AtomicInteger counter;
   public ThreadHelperImpl() {
+      counter = new AtomicInteger();
+      this.keysupplier = () -> "Thread #" + counter.incrementAndGet();
   }
 
+  public ThreadHelperImpl(Supplier<String> keysupplier) {
+    counter = null;
+    this.keysupplier = keysupplier;
+  }
+  
   @Override
-  public Thread newThread(Runnable runnable, String key) {
-    System.out.println("Using platofrm threads");
-    return Thread.ofPlatform().name(key).unstarted(runnable);
+  public Thread newThread(Runnable runnable) {
+    return Thread.ofPlatform().name(keysupplier.get()).unstarted(runnable);
   }
-
 }
